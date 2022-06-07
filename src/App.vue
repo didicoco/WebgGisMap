36768e6c5d13e8a6488553ee69c65b2941e0b1d6
didicoco <924269612qq.com>
2022-06-07 16:17:48 +0800

    增加element ui 框架布局

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,42 @@
 <template>
-  <div id="app">
- 
-  </div>
+    <div id="app">
+        <el-container class="app-out-pannel">
+            <el-header class="sys-header">一张图项目系统</el-header>
+            <el-container class="app-content-pannel">
+                <el-aside width="200px" class="sys-menu">左侧菜单</el-aside>
+                <el-main>地图区域</el-main>
+            </el-container>
+        </el-container>
+    </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'App',
-  components: {
-   
-  }
-}
+    name: 'App',
+    components: {},
+};
 </script>
 
 <style>
-html,body,#app {
- position: relative;
- width: 100%;
- height: 100%;
- margin: 0;
+html,
+body,
+#app {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+}
+.app-out-pannel,
+.app-content-pannel {
+    height: 100%;
+}
+.sys-header {
+    background-color: #409eff;
+    line-height: 60px;
+    color: #fff;
+    font-size: 20px;
+}
+.sys-menu {
+    background-color: #c0c4cc;
 }
 </style>
