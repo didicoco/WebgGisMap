42b707d8d218559266cd6b0708b97625a5f013cc
didicoco <924269612qq.com>
2022-08-15 15:37:00 +0800

    verson9.1用户登陆功能接口开发

--- node/index.js
+++ node/index.js
@@ -2,8 +2,22 @@ var express = require('express');//异步调用，AMD
 var app = express();//express方法的调用生成一个app实例
 var user = require('./routers/user');
 var home = require('./routers/home');
+//设置跨域访问（一般是后端处理跨域，前端可以用代理解决跨域）
+app.all("*", function (req, res, next) {
+    //设置允许跨域的域名，*代表允许任意域名跨域
+    res.header("Access-Control-Allow-Origin", "*");
+    //允许的header类型
+    res.header("Access-Contro1-Allow-Headers", "content-type");
+    //跨域允许的请求方式
+    res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
+    if (req.method.toLowerCase() == 'options ')
+        res.send(200);//让options尝试请求快速结束else
+    next();
+})
+
 
 app.use('/', home);
 app.use('/user', user);
-
 app.listen(3001);//监听3001的端口
+
+
--- node/routers/user.js
+++ node/routers/user.js
@@ -1,10 +1,12 @@
 var express = require('express');//引入express框架
 var pg = require('pg');
 var router = express.Router();//router是用来编写接口的
-var pgConfig = "postgres://postgres:0608222@localhost:5432/webgis";
+var pgConfig = "postgres://postgres:0608222@localhost:5432/webgis";//连接pg数据库
 
 
+//获取用户信息
 router.get('/get', function (req, res) {
+    var name = req.query.name;
     var client = new pg.Client(pgConfig);
     client.connect(function (isErr) {
         if (isErr) {
@@ -12,11 +14,7 @@ router.get('/get', function (req, res) {
             client.end();
             return;
         }
-        const sql = {
-            text: "SELECT * FROM public.user where name = $1",
-            values: ['admin']
-        }
-        client.query(sql, function (isErr, rst) {
+        client.query('SELECT * FROM public.user where name =$1', [name], function (isErr, rst) {
             if (isErr) {
                 console.log('query error:' + isErr.message);
                 res.send({
@@ -32,12 +30,37 @@ router.get('/get', function (req, res) {
                 });
             }
             client.end();
-        })
-    })
+        });
+    });
 });
 
+//用户注册接口
 router.post('/insert', function (req, res) {
-    res.send('user insert');
+    var client = new pg.Client(pgConfig);
+    client.connect(function (isErr) {
+        if (isErr) {
+            console.log('connect error' + isErr.message);
+            client.end();
+            return;
+        }
+        client.query('INSERT INTO public."user" (name, password, phone, email) VALUES ($1, $2, $3, $4)', ['xie', '123456', '17625922185', '398514470@qq.com'], function (isErr, rst) {
+            if (isErr) {
+                console.log('insert error:' + isErr.message);
+                res.send({
+                    status: 'fail',
+                    msg: 'insert error'
+                });
+            } else {
+                console.log('insert success,data is:' + rst);
+                console.log(JSON.parse(JSON.stringify(rst)));
+                res.send({
+                    status: 'success',
+                    data: []
+                });
+            }
+            client.end();
+        });
+    });
 });
 
 module.exports = router;
